5df03fd12d2cfd0670c77548b330ac8dfcde47e0
pbrehill <pbrehill@gmail.com>
2024-01-28 18:15:48 +1100

    Allowed event system to talk to emails. There is test code in there for if comedians bomb or succeed a gig which sends an email.

--- Assets/Scripts/DesktopVisuals/Clock.cs
+++ Assets/Scripts/DesktopVisuals/Clock.cs
@@ -12,10 +12,13 @@ public class Clock : MonoBehaviour
     public TextMeshProUGUI TimeDisplay;
 
     private int HoursPassed = 9;
+
+    public int AllHours = 0;
+    private EmailManager emailManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        emailManager = GameObject.Find("Window - Email App").GetComponent<EmailManager>();
     }
 
     // Update is called once per frame
@@ -24,16 +27,45 @@ public class Clock : MonoBehaviour
         if (tickTimer >= HourTickTime)
         {
             HoursPassed++;
+            AllHours++;
             if (HoursPassed > 17)
             {
                 FindObjectOfType<ChuckleHubManager>().EndDay();
                 HoursPassed = 9;
             }
 
+             // Testing
+            if (AllHours == 10)
+            {
+                Debug.Log("RUNNING TEST");
+                Dictionary<string, string> businessEmail = new Dictionary<string, string>
+            {
+                {"from", "a.johnson@enterprise.org"},
+                {"subject", "Meeting Schedule"},
+                {"body", "Please review the attached document for the budget review details."}
+            };
+            emailManager.GeneratedEmail(businessEmail);
+            }
+
+            emailManager.RefreshEmails();
+
             tickTimer = 0;
         }
 
         tickTimer += Time.deltaTime;
         TimeDisplay.text = HoursPassed + ":00";
+    }
+
+    public string FormattedTime()
+    {
+        // Get a random minute value
+        int randomNumber = UnityEngine.Random.Range(1, 60); // Generates a number between 1 and 59
+        string randomMinute = randomNumber.ToString("D2");
+
+        // Get day and time
+        string displayTime = HoursPassed + ":" + randomMinute;
+        int day = (AllHours / 9) + 1;
+
+        return $"{displayTime} {day.ToString()}/3";
     }
 }
--- Assets/Scripts/EmailSystem/EmailManager.cs
+++ Assets/Scripts/EmailSystem/EmailManager.cs
@@ -17,13 +17,20 @@ public class EmailManager : MonoBehaviour
     public Transform contentTransform;
     private Dictionary<string, List<string>> emailCsv;
     int numberOfRows;
+    private Clock clock;
 
     void Start()
     {
+        // Link up to clock
+        clock = GameObject.Find("Clock").GetComponent<Clock>();
+
+        // Load in the premade emails
         email_text.text = "";
         emailCsv = TSVReader.ReadTSV("fbemails.txt");
         numberOfRows = emailCsv.First().Value.Count;
         emailCsv["loadedAlready"] = Enumerable.Repeat("FALSE", numberOfRows).ToList();
+
+        // Get emails
         RefreshEmails();
     }
 
@@ -48,7 +55,7 @@ public class EmailManager : MonoBehaviour
 
     public void DisplayContentForIcon(string id)
     {
-        int idIndex = SearchEmails(emailCsv, id);
+        int idIndex = SearchEmails(id);
         string tmp_text = $@"From: {emailCsv["from"][idIndex]}
 Sent: {emailCsv["msgtime"][idIndex]}
 To: Manager
@@ -62,34 +69,34 @@ Subject: {emailCsv["subject"][idIndex]}
 
     // This runs whenever we need to get new emails.
     // Right now I just use time, I'll add better logic later.
-    public void AdvanceTime()
-    {
-        time++;
-        // This is temporary, but this is a public method, can be used by other objects.
-        if (time == 1) {
-            SendEmail("jackie1");
-            SendEmail("jetstar1");
-            SendEmail("SICF1");
-            SendEmail("slambo1");
-            SendEmail("mum1");
-        }
-        if (time == 2) {
-            SendEmail("chuckle2");
-            SendEmail("witty1");
-            SendEmail("chuckle3");
-            SendEmail("mum2");
-            SendEmail("mum3");
-        }
-        Debug.Log($"Time is {time}.");
+    // public void AdvanceTime()
+    // {
+    //     time++;
+    //     // This is temporary, but this is a public method, can be used by other objects.
+    //     if (time == 1) {
+    //         SendEmail("jackie1");
+    //         SendEmail("jetstar1");
+    //         SendEmail("SICF1");
+    //         SendEmail("slambo1");
+    //         SendEmail("mum1");
+    //     }
+    //     if (time == 2) {
+    //         SendEmail("chuckle2");
+    //         SendEmail("witty1");
+    //         SendEmail("chuckle3");
+    //         SendEmail("mum2");
+    //         SendEmail("mum3");
+    //     }
+    //     Debug.Log($"Time is {time}.");
         
-        RefreshEmails();
-    }
+    //     RefreshEmails();
+    // }
 
-    void RefreshEmails() {
+    public void RefreshEmails() {
 
         for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++)
         {
-            if (emailCsv["conditions"][rowIndex] == "TRUE" & emailCsv["loadedAlready"][rowIndex] == "FALSE"){
+            if (emailCsv["conditions"][rowIndex] == clock.AllHours.ToString() & emailCsv["loadedAlready"][rowIndex] == "FALSE"){
                 spawnEmail(rowIndex);
             }
         }
@@ -105,29 +112,72 @@ Subject: {emailCsv["subject"][idIndex]}
         script.iconID = emailCsv["id"][rowIndex];
         script.txtFrom.text = emailCsv["from"][rowIndex];
         script.txtSubject.text = emailCsv["subject"][rowIndex];
-        script.txtTime.text = emailCsv["msgtime"][rowIndex];
+        script.txtTime.text = clock.FormattedTime();
         emailCsv["loadedAlready"][rowIndex] = "TRUE";
 
         return;
     }
 
-    int SearchEmails(Dictionary<string, List<string>> emails, string searchId)
+    int SearchEmails(string searchId)
     {
         for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-            if (emails["id"][rowIndex] == searchId) {
+            if (emailCsv["id"][rowIndex] == searchId) {
                 return rowIndex;
             }
         }
-        throw new Exception($"Error: id {searchId} not found in email list.");
+
+        return -1;
     }
 
-    public void SendEmail(string idToActivate, bool sendASAP = false)
+    // public void SendEmail(string idToActivate)
+    // {
+    //     int indexToActivate = SearchEmails(idToActivate);
+    //     // Excuse this DISGUSTING behaviour
+    //     emailCsv["conditions"][indexToActivate] = clock.AllHours + 1;
+    // }
+
+    public void GeneratedEmail(Dictionary<string, string> newEntry)
     {
-        int indexToActivate = SearchEmails(emailCsv, idToActivate);
-        // Excuse this DISGUSTING behaviour
-        emailCsv["conditions"][indexToActivate] = "TRUE";
-        if (sendASAP) {
-            RefreshEmails();
+        // Just checking that we have the right keys
+        var requiredKeys = new HashSet<string> { "from", "subject", "body" };
+
+        bool containsOnlyRequiredKeys = newEntry.Count == requiredKeys.Count && 
+                                        requiredKeys.All(key => newEntry.ContainsKey(key));
+        if (!containsOnlyRequiredKeys) {
+            throw new Exception("The dictionary does not just have the required keys");
+        }
+        
+        // Add to the email data structure
+        foreach (KeyValuePair<string, string> kvp in newEntry)
+        {
+            emailCsv[kvp.Key].Add(kvp.Value);
+        }
+        
+        // Create placeholder time
+        emailCsv["msgtime"].Add(clock.FormattedTime());
+
+        // Set loaded already
+        emailCsv["loadedAlready"].Add("FALSE");
+
+        // Create a unique ID
+        // Terrible hack but running out of time.
+        string tempID = newEntry["from"] + newEntry["subject"] + UnityEngine.Random.Range(1, 1000000).ToString();
+        emailCsv["id"].Add(tempID);
+        
+        // Set send time as shortly in future to send.
+        emailCsv["conditions"].Add((clock.AllHours + 1).ToString());
+
+        // Increment number of rows
+        numberOfRows++;
+
+        Debug.Log("Run test");
+        Debug.Log($"New index {numberOfRows - 1}, total length {numberOfRows}.");
+
+        Debug.Log($"New index {numberOfRows - 1}, total length {numberOfRows}.");
+
+        foreach (KeyValuePair<string, List<string>> kvp in emailCsv)
+        {
+            Debug.Log($"{kvp.Key}: ");
         }
     }
 }
